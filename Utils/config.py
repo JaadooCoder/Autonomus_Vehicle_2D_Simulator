18ce801b83e8bad5a6e0a870c0e524207b8beb47
JaadooCoder <jha.adway1604@gmail.com>
2026-05-26 12:31:04 +0530

    Added metadata of objects

--- Utils/config.py
+++ Utils/config.py
@@ -3,3 +3,37 @@ This script contains the metadata of the world. The details of the windows, the 
 
 Handled by - Adway
 """
+
+# Window configuration
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 700
+
+FPS = 60
+
+
+# Colors
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+ROAD_COLOR = (60,60,60)
+
+GRASS_COLOR = (30,120,30)
+
+LANE_COLOR = (255,255,255)
+
+CAR_COLOR = (255,0,0)
+
+
+# Road settings
+
+ROAD_WIDTH = 300
+
+
+# Car settings
+
+CAR_WIDTH = 40
+CAR_HEIGHT = 60
+
+CAR_SPEED = 5
