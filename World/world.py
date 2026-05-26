ae43e3edfd4e33c6eb7352bbab13bf0041ec445f
JaadooCoder <jha.adway1604@gmail.com>
2026-05-26 12:45:56 +0530

    Updating Headers

---
+++ World/world.py
@@ -0,0 +1,5 @@
+""" 
+This script manages the simulation world of the autonomous vehicle platform. It is responsible for creating and maintaining the environment where all subsystems operate. The world initializes the simulator window, controls the main game loop, handles eventsupdates world states, and renders all objects such as roads, vehicles, obstacles, and future environment elements.
+
+Handled by - Adway
+"""
