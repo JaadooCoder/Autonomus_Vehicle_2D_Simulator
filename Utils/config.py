77aad26a9731a0e137f947b71210d206951a3fe3
JaadooCoder <jha.adway1604@gmail.com>
2026-05-26 12:29:08 +0530

    Updating Headers

---
+++ Utils/config.py
@@ -0,0 +1,5 @@
+""" 
+This script contains the metadata of the world. The details of the windows, the colours of objects, the dimensions of obsctacles, etc, will be defined here. This is the file which handles the configurations of everything in the world. Any changes related to appearance and behaviour of the world will be made here. 
+
+Handled by - Adway
+"""
--- World/world.py
+++
@@ -1,6 +0,0 @@
-""" This will contain script related to the environment create dusing PyGame. The city, the roads, the car, the vehicles, the pedestrians, and everything that is found in real life will be created and manipulated here.
-
-This script will contain the default behaviour patterns of obstacles, and by changing these values will we be able to make them follow algorithmic behaviours. 
-
-Handled by - Adway
-"""
