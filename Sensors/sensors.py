af6505868a54ce591dcdc12645cb4422a49862ad
JaadooCoder <jha.adway1604@gmail.com>
2026-06-16 20:25:21 +0530

    Updating Headers

---
+++ Sensors/sensors.py
@@ -0,0 +1,6 @@
+"""
+This script simulates the sensor suite of the autonomous vehicle. It collects information from the environment and converts it into readable data that can be used by perception and decision-making systems.
+
+Handled by - Adway
+"""
+
